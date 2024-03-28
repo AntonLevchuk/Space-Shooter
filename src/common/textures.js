3762d855571cd99f3b28bfa3ca4cf9fb9bbde91a
Anton <antonlevchuk626@gmail.com>
2024-03-28 20:01:46 +0200

    remove useless parts

--- src/common/textures.js
+++ src/common/textures.js
@@ -1,36 +1,12 @@
 export const allTextureKeys = {
   shipBoss: "shipBoss",
-  bomb: "bomb",
-  // musicOn: "musicOn",
-  // musicOff: "musicOff",
-  // effectsOn: "effectsOn",
-  // effectsOff: "effectsOff",
-  enemyShip: "enemyShip",
   spaceShip: "spaceShip",
-  bullet1: "bullet1",
-  bullet2: "bullet2",
-  explosion: "explosion",
-  explosion1: "explosion1",
-  explosion2: "explosion2",
-  explosion3: "explosion3",
   asteroid: "asteroid",
 };
 
 const appTextures = {
     [allTextureKeys.shipBoss]: 'assets/sprites/enemy/shipBoss.png',
-    [allTextureKeys.bomb]: 'assets/sprites/bomb.png',
-    // [allTextureKeys.musicOn]: 'assets/sprites/ui/music_on.png',
-    // [allTextureKeys.musicOff]: 'assets/sprites/ui/music_off.png',
-    // [allTextureKeys.effectsOn]: 'assets/sprites/ui/volume_on.png',
-    // [allTextureKeys.effectsOff]: 'assets/sprites/ui/volume_off.png',
-    [allTextureKeys.enemyShip]: 'assets/sprites/enemy/shipBoss.png',
     [allTextureKeys.spaceShip]: 'assets/sprites/player.png',
-    [allTextureKeys.bullet1]: 'assets/sprites/bullets/bullet1.json',
-    [allTextureKeys.bullet2]: 'assets/sprites/bullets/bullet2.json',
-    [allTextureKeys.explosion]: 'assets/sprites/explosion/explosion.json',
-    [allTextureKeys.explosion1]: 'assets/sprites/explosion/explosion1.json',
-    [allTextureKeys.explosion2]: 'assets/sprites/explosion/explosion2.json',
-    [allTextureKeys.explosion3]: 'assets/sprites/explosion/explosion3.json',
     [allTextureKeys.asteroid]: 'assets/sprites/asteroids/asteroid.png',
 };
 
